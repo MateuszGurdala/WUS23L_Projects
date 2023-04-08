9c82183d781d94e726556f1231d8e4109cc1f83d
amisiak1 <amisiak1@gitlab-stud.elka.pw.edu.pl>
2023-04-08 18:40:16 +0200

    Front end possible fix

--- Lab I/deploy/front.sh
+++ Lab I/deploy/front.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-# Checks if the script was called with three arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 SERVER_IP SERVER_PORT FRONT_PORT" >&2
-    exit 1
-fi
-
 SERVER_IP="$1"
 SERVER_PORT="$2"
-FRONT_PORT="$3"
+FRONTEND_PORT="$3"
 
-# Installs necessary packages and updates the system
+# Init packages
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt autoremove -y
 sudo apt-get install curl -y
 sudo apt-get install npm -y
 
-export HOME=/root
+cd ~ 
 
-# Installs the nvm sets the environment variables and installs version Node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+# Install Node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 12.11.1
 
-# Clones the Spring PetClinic Angular project from GitHub
+nvm install 16.14.2
+
+# Petclinic forntend clone
 cd ~/
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
 cd spring-petclinic-angular/
@@ -35,17 +30,18 @@ cd spring-petclinic-angular/
 sed -i "s/localhost/$SERVER_IP/g" src/environments/environment.ts src/environments/environment.prod.ts
 sed -i "s/9966/$SERVER_PORT/g" src/environments/environment.ts src/environments/environment.prod.ts
 
-# Installs the latest version of the Angular CLI and necessary packages
+# Install angular
 echo N | npm install -g @angular/cli@latest
 echo N | npm install
 echo N | ng analytics off
 
 npm install angular-http-server
+npm audit fix
 
 # Build project in production mode.
 npm run build -- --prod
 
 # Start server using angular-http-server
-npx angular-http-server --path ./dist -p $FRONT_PORT &
+npx angular-http-server --path ./dist -p $FRONTEND_PORT &
 
 echo DONE
