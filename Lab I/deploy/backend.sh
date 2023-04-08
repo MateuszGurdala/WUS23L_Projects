91ee420dc94585ad6a25342a113ef1dedc973faf
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-04-08 18:39:47 +0200

    Fixed backend deploy script

--- Lab I/deploy/backend.sh
+++ Lab I/deploy/backend.sh
@@ -6,19 +6,27 @@ database_port=$2
 backend_port=$3
 
 #Update denviroment
-cd /root
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y default-jre
-sudo apt install git
+
+# sudo apt install -y default-jre -> installs jre-11
+sudo apt install openjdk-17-jre -y
+sudo apt install maven -y
+sudo apt install git -y
 
 #Clone spring-petclinic-rest
+cd /home
+HOME_DIR="$(ls)"
+HOME_PATH="/home/$HOME_DIR"
+cd $HOME_PATH
 git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
 cd spring-petclinic-rest
 
-#Change database ip, database port and bacdkend port
-sed -i "s/localhost/$database_ip/g" src/main/resources/application-mysql.properties
-sed -i "s/3306/$database_port/g" src/main/resources/application-mysql.properties
-sed -i "s/9966/$backend_port/g" src/main/resources/application.properties
-sed -i "s/active=hsqldb/active=mysql/g" src/main/resources/application.properties
-./mvnw spring-boot:run &
+#Change database ip, database port and backend port
+sudo sed -i "s/localhost/$database_ip/g" src/main/resources/application-mysql.properties
+sudo sed -i "s/3306/$database_port/g" src/main/resources/application-mysql.properties
+sudo sed -i "s/9966/$backend_port/g" src/main/resources/application.properties
+sudo sed -i "s/active=hsqldb/active=mysql/g" src/main/resources/application.properties
+
+#Run backend
+sudo ./mvnw spring-boot:run &
