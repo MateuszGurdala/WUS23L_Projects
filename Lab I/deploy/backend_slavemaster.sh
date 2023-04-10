14411399e2b9eb75e19ac3026b2b39db06d357eb
amisiak1 <amisiak1@gitlab-stud.elka.pw.edu.pl>
2023-04-10 12:41:13 +0200

    Changes in progress

--- Lab I/deploy/backend_slavemaster.sh
+++ Lab I/deploy/backend_slavemaster.sh
@@ -25,7 +25,7 @@ git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
 cd spring-petclinic-rest
 
 #Change database ip, database port and backend port
-sudo sed -i "s/localhost:3306/${database_ip}:${database_port},${database_slave_ip}:${database_slave_port}/" src/main/resources/application-mysql.properties
+sudo sed -i "s/localhost:3306/$database_ip:$database_port,$database_slave_ip:$database_slave_port/" src/main/resources/application-mysql.properties
 sudo sed -i "s/9966/$backend_port/g" src/main/resources/application.properties
 sudo sed -i "s/active=hsqldb/active=mysql/g" src/main/resources/application.properties
 sudo sed -i 's/username=.*/username=pc/' src/main/resources/application-mysql.properties
