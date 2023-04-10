50d771d57dc6e2d4b8f285d8f2d9065bd01f0558
Maciej Łodziński <85826774+mflodzinski@users.noreply.github.com>
2023-04-10 21:51:23 +0200

    Update database.sh

--- Lab_I/deploy/database.sh
+++ Lab_I/deploy/database.sh
@@ -6,37 +6,13 @@ MASTER_PORT="$1"
 sudo apt update -y
 sudo apt install -y mysql-server wget
 
+cd /home
 
-echo "[mysqld]" | sudo tee -a /etc/mysql/my.cnf
-echo "port=$MASTER_PORT" | sudo tee -a /etc/mysql/my.cnf
-
-#INTERFACE="eth0"
-#ip_info=$(ifconfig "$INTERFACE")
-#private_ip=$(echo "$ip_info" | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -vE '^127\.')
-#echo "bind-address = $private_ip" | sudo tee -a /etc/mysql/my.cnf
-
-echo "server-id = 1" | sudo tee -a /etc/mysql/my.cnf
 sudo sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo sed -i "s/3306/$MASTER_PORT/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
-sudo chmod -R u+rwx /etc/mysql
-sudo chown -R mysql.mysql /etc/mysql
-
-sudo chown -R mysql /var/lib/mysql
-sudo chgrp -R mysql /var/lib/mysql
-sudo chmod 755 /var/lib/mysql
-
-
-sudo chown -R mysql /var/log/mysql
-sudo chgrp -R mysql /var/log/mysql
-sudo chmod 755 /var/log/mysql
-sudo chown -R mysql /var/log/mysql/error.log
-sudo chgrp -R mysql /var/log/mysql/error.log
-sudo chmod 755 /var/log/mysql/error.log
-
 sudo mysql -e "CREATE USER 'master'@'%' IDENTIFIED BY 'passwd'; GRANT ALL PRIVILEGES ON *.* TO 'master'@'%' WITH GRANT OPTION;"
 sudo mysql -e "CREATE USER 'pc'@'%' IDENTIFIED BY 'petclinic'; GRANT ALL PRIVILEGES ON *.* TO 'pc'@'%' WITH GRANT OPTION;"
-
 
 wget "https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/initDB.sql"
 wget "https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/populateDB.sql"
@@ -50,7 +26,6 @@ sudo sed -i "s/.*log_bin.*/log_bin = \\/var\\/log\\/mysql\\/mysql-bi.log/" /etc/
 
 sudo mysql -e "CREATE USER 'replicate'@'%' IDENTIFIED BY 'passwd'; GRANT REPLICATION SLAVE ON *.* TO 'replicate'@'%';"
 sudo mysql -e "FLUSH PRIVILEGES;"
-sudo mysql -v -e "UNLOCK TABLES;"
+
 
 sudo service mysql restart
-
