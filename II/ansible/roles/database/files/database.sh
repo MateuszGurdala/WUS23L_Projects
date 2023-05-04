02ea3c57cd59af5048829980a234a13361ddd0f8
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-05-05 00:39:17 +0200

    Finished database role

--- II/ansible/roles/database/files/database.sh
+++ II/ansible/roles/database/files/database.sh
@@ -17,11 +17,11 @@ sudo sed -i "s/3306/$MASTER_PORT/" /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo mysql -e "CREATE USER 'master'@'%' IDENTIFIED BY 'passwd'; GRANT ALL PRIVILEGES ON *.* TO 'master'@'%' WITH GRANT OPTION;"
 sudo mysql -e "CREATE USER 'pc'@'%' IDENTIFIED BY 'petclinic'; GRANT ALL PRIVILEGES ON *.* TO 'pc'@'%' WITH GRANT OPTION;"
 
-wget "https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/initDB.sql"
-wget "https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/populateDB.sql"
+sudo wget "https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/initDB.sql"
+sudo wget "https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/populateDB.sql"
 
-sudo mysql < initDB.sql
-sudo mysql --database=petclinic < populateDB.sql
+sudo mysql <initDB.sql
+sudo mysql --database=petclinic <populateDB.sql
 
 sudo sed -i "s/.*server-id.*/server-id = 1/" /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo sed -i "s/.*log_bin.*/log_bin = \\/var\\/log\\/mysql\\/mysql-bi.log/" /etc/mysql/mysql.conf.d/mysqld.cnf
